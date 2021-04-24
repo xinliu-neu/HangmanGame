63d2c3a81079c56c6357985005d7610269e46646
Xin Liu <liu.xin7@northeastern.edu>
2021-04-23 19:13:36 -0700

    Drafted the project.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
 import './App.css';
 
+
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+let playable = true
+
+// const correctLetter = [];
+// const wrongLetters = [];
+
+
 function App() {
+  const [playable, setPlayable] = useState([]);
+  const [correctLetters, setCorrectLetters] = useState([])
+  const [wrongtLetters, setWrongLetters] = useState([])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header />
+        <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+        </div>
+      </>
   );
 }
 
---
+++ src/components/Word.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Word ({selectedWord, correctLetters }) {
+  return (
+      <div>
+      </div>
+  )
+}
+export default Word
